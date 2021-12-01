6053dd5ba3d5bae422a777476ba8e02eae7fe7e6
Douglas Souza <douglassouzabjj@hotmail.com>
2021-12-01 10:07:21 -0300

    Modificaçao

--- bloco-4/bloco-4.3/logica_de_programacao.js
+++ bloco-4/bloco-4.3/logica_de_programacao.js
@@ -7,3 +7,5 @@
 // Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
 
 // Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
+
+// Modificação
