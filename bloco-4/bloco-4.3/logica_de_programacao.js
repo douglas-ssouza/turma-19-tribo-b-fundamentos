f1af05158a86f6059dc43a8e15f218643440f35a
Douglas Souza <douglassouzabjj@hotmail.com>
2021-12-01 08:43:48 -0300

    Exercicios para pratica de logica de programaçao

---
+++ bloco-4/bloco-4.3/logica_de_programacao.js
@@ -0,0 +1,9 @@
+// Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+// Dado um array de numeros. Encontre o maior valor e a sua posição.
+
+// Leia um valor inteiro, que é o tempo de duração em horas de um determinado evento em uma fábrica, e informe-o em segundos
+
+// Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+// Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
