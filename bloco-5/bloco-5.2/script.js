1edc073eaa79916c88607af2ceab96dc365b0fc9
Douglas Souza <douglassouzabjj@hotmail.com>
2021-12-06 20:23:11 -0300

    Cria pagina para trabalho

---
+++ bloco-5/bloco-5.2/script.js
@@ -0,0 +1,46 @@
+const estados = [
+  { sigla: 'AC', nome: 'Acre' },
+  { sigla: 'AL', nome: 'Alagoas' },
+  { sigla: 'AP', nome: 'Amapá' },
+  { sigla: 'AM', nome: 'Amazonas' },
+  { sigla: 'BA', nome: 'Bahia' },
+  { sigla: 'CE', nome: 'Ceará' },
+  { sigla: 'DF', nome: 'Distrito Federal' },
+  { sigla: 'ES', nome: 'Espírito Santo' },
+  { sigla: 'GO', nome: 'Goías' },
+  { sigla: 'MA', nome: 'Maranhão' },
+  { sigla: 'MT', nome: 'Mato Grosso' },
+  { sigla: 'MS', nome: 'Mato Grosso do Sul' },
+  { sigla: 'MG', nome: 'Minas Gerais' },
+  { sigla: 'PA', nome: 'Pará' },
+  { sigla: 'PB', nome: 'Paraíba' },
+  { sigla: 'PR', nome: 'Paraná' },
+  { sigla: 'PE', nome: 'Pernambuco' },
+  { sigla: 'PI', nome: 'Piauí' },
+  { sigla: 'RJ', nome: 'Rio de Janeiro' },
+  { sigla: 'RN', nome: 'Rio Grande do Norte' },
+  { sigla: 'RS', nome: 'Rio Grande do Sul' },
+  { sigla: 'RO', nome: 'Rondônia' },
+  { sigla: 'RR', nome: 'Roraíma' },
+  { sigla: 'SC', nome: 'Santa Catarina' },
+  { sigla: 'SP', nome: 'São Paulo' },
+  { sigla: 'SE', nome: 'Sergipe' },
+  { sigla: 'TO', nome: 'Tocantins' },
+];
+
+const statesTable = document.querySelector('.states-table');
+
+for (let index = 0; index < estados.length; index += 1) {
+  const linha = document.createElement('tr');
+  
+  const nome = document.createElement('td');
+  const sigla = document.createElement('td');
+  
+  nome.innerText = estados[index].nome;
+  sigla.innerText = estados[index].sigla;
+
+  linha.appendChild(nome);
+  linha.appendChild(sigla);
+
+  statesTable.lastElementChild.appendChild(linha);
+}
