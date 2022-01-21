e9d2bcdb06cb06cf5596f0f074acd056fb0d4ef0
Douglas Souza <douglassouzabjj@hotmail.com>
2022-01-21 08:42:19 -0300

    Adiciona desafios

--- bloco-8/bloco-8.3/filter.js
+++ bloco-8/bloco-8.3/filter.js
@@ -1,2 +1,21 @@
 const { numbers, names, students } = require('./arrays');
 
+// Filtre o array numbers por números positivos (incluindo 0)
+
+// Filtre o array numbers por número negativos
+
+// Filtre o array numbers por números pares/impares
+
+// Filtre o array names por nomes que comecem com a letra R/r
+
+// Filtre o array names por nomes que tenham mais de 5 caracteres
+
+// Filtre o array names por nomes que incluam a letra a
+
+// Filtre o array students por pessoas com idade maior/menor que 30
+
+// Filtre o array students por pessoas com CSS como uma tech favorita
+
+// Filtre o array students por pessoas com Jest e React como tech favoritas
+
+// Filtre o array students por pessoas com JavaScript ou Jest como techs favoritas
--- bloco-8/bloco-8.3/map.js
+++ bloco-8/bloco-8.3/map.js
@@ -1,1 +1,13 @@
 const { numbers, names, students } = require('./arrays');
+
+// Gere um novo array com o dobro/metade de cada item do array numbers
+
+// Gere um novo array com quadrado de cada item do array numbers
+
+// Gere um novo array de names alterando todas as letras 'e' por 'x'
+
+// Gere um novo array apenas com as idades das pessoas estudantes
+
+// Gere um novo array de students com os nomes completos das pessoas estudantes
+
+// Gere um novo array 
