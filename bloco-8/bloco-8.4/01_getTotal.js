7c22bb827c2a8f112525541f788faccfab04b399
Douglas Souza <douglassouzabjj@hotmail.com>
2022-01-24 12:38:20 -0300

    Resoluçao dos exercicios

--- bloco-8/bloco-8.4/01_getTotal.js
+++ bloco-8/bloco-8.4/01_getTotal.js
@@ -1,11 +1,13 @@
 const numbers = [0, 4, 6, 1, 3, 4, 7, 9, 2, 18, 200, 2];
 
 const getTotal = (array) => {
-  let total = 0;
-  for (let i = 0; i < array.length; i += 1) {
-    total += array[i];
-  }
-  return total;
+  // let total = 0;
+  // for (let i = 0; i < array.length; i += 1) {
+  //   total += array[i];
+  // }
+  // return total;
+
+  return array.reduce((acumulador, atual) => acumulador + atual);
 }
 
 console.log(getTotal(numbers));
